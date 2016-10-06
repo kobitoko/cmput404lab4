05458e1402d6d994ef444e794c7f5a6a6aa7b358
kobitoko <kobitoko@gmail.com>
2016-10-06 15:19:17 -0600

    added a comment for my own reference

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # Note the r'^(?P<question_id>[0-9]+)/$' and r'^(?P<question_id>[0-9]+)$' <- (missing the end "/"). 
     # are different, if u do without the "/" it 404s
     # Also add [A-Z] or [a-z] to allow alphabetical characters.
+    #pk is short for PrimaryKey
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
